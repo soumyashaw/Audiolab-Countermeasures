7cfcc20fc5f2d29a469b3d4cb093ff72d11c5661
Soumya Shaw <37841510+soumyashaw@users.noreply.github.com>
2024-08-10 18:27:14 +0200

    Update sti.py

--- src/package_name/sti.py
+++ src/package_name/sti.py
@@ -260,7 +260,7 @@ def octaveBandSpectra(filteredAudioBands, hz, fftRes=0.06):
     """
 
     # FFT window size for PSD calculation: 32768 for ~0.06 Hz res at 2 kHz
-    psdWindow = fftWindowSize(fftRes, hz)
+    psdWindow = int(fftWindowSize(fftRes, hz)/1024)
     
     print("Calculating octave band power spectras")
     print("(FFT length:", psdWindow, "samples)")
