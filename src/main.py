97329b75109fc76fc0f1a2a30195955b4d77d08a
Soumya Shaw <37841510+soumyashaw@users.noreply.github.com>
2024-08-10 17:45:12 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
+import os
 import argparse
 from simple_term_menu import TerminalMenu
-#from package_name.sti import stiFromAudio, readwav
+from package_name.sti import stiFromAudio, readwav
 
 def main():
     # Define the menu options
@@ -24,6 +25,13 @@ def main():
             print("Calculating Average STI...")
             print("Target Directory: ", args.target_dir)
             print("Reference Directory: ", args.reference_dir)
+
+            target_files = os.listdir(args.target_dir)
+            reference_files = os.listdir(args.reference_dir)
+            if target_files == reference_files:
+                print("Equal")
+            else:
+                print("Not Equal")
             
 
         elif selected_option_index == 1:
@@ -39,7 +47,7 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-t', '--target_dir', type=str, help="path to the target audio's directory", default="/hkfs/home/haicore/hgf_cispa/hgf_yie2732/BaselineDataset/LA/ASVspoof2019_LA_eval/lpf")
+    parser.add_argument('-t', '--target_dir', type=str, help="path to the target audio's directory", default="/hkfs/home/haicore/hgf_cispa/hgf_yie2732/BaselineDataset/LA/ASVspoof2019_LA_eval/lpf/")
     parser.add_argument('-r', '--reference_dir', type=str, help="path to the reference audio's directory", default="/hkfs/home/haicore/hgf_cispa/hgf_yie2732/BaselineDataset/LA/ASVspoof2019_LA_eval/original_wav/")
     args = parser.parse_args()
     main()
