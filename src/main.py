2c5859f62a01c7cded0b0e2cc98e393207b78fa8
Soumya Shaw <37841510+soumyashaw@users.noreply.github.com>
2024-08-14 15:37:41 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -152,12 +152,13 @@ def add_codec_loss(audioPath, format, codec: str):
 
         return audio
     
-def add_reverberation(audioPath:str, targetpath: str):
-    cmd1 = "ffmpeg -loglevel error -i " + audioPath + " -map 0 -c:v copy -af aecho=1.0:0.9:70:0.5 " + targetpath
-    os.system(cmd1)
-
-    cmd2 = "ffmpeg -loglevel error -i " + audioPath + " -i ../data/iir.wav -filter_complex '[0] [1] afir=dry=10:wet=10 [reverb]; [0] [reverb] amix=inputs=2:weights=10 4' " + targetpath
-    os.system(cmd2)
+def add_reverberation(audioPath:str, targetpath: str, selectable: int = 0, iir_path: str = "/hkfs/home/haicore/hgf_cispa/hgf_yie2732/Audiolab-Countermeasures/data/iir.wav"):
+    if selectable == 0:
+        cmd1 = f"ffmpeg -loglevel error -i {audioPath} -map 0 -c:v copy -af aecho=1.0:0.9:70:0.5 {targetpath}"
+        os.system(cmd1)
+    elif selectable == 1:
+        cmd2 = f"ffmpeg -loglevel error -i {audioPath} -i {iir_path} -filter_complex '[0] [1] afir=dry=10:wet=10 [reverb]; [0] [reverb] amix=inputs=2:weights=10 4' {targetpath}"
+        os.system(cmd2)
 
 
 
@@ -334,20 +335,30 @@ def main():
                 print(" "*50 + "\033[91mAdding Reverberation\033[0m")
                 print()
 
+                # Collect the audio files from the reference directory
                 audio_files = os.listdir(args.reference_dir)
 
+                # Divide the audio files into two partitions for reverb effects
                 audio_files = divide_list_randomly(audio_files, 2)
 
+                # Change the directory to the reference directory
                 os.chdir(args.reference_dir)
 
                 os.chdir("../")
 
+                # Make a directory to store the augmented data
                 make_directory(os.getcwd() + "/augmented_data/reverberations/")
 
                 for i in range(len(audio_files)):
-                    print("Segement: ", i)
+                    for audio in tqdm(audio_files[i], desc="Adding Reverberation to Partition " + str(i+1)):
+                        input_audio = args.reference_dir + str(audio)
+                        output_audio = os.getcwd() + "/augmented_data/reverberations/" + "reverb_" + str(audio)
 
-                print(os.getcwd())
+                        # Call the function to add reverb effects to the audio file
+                        add_reverberation(input_audio, output_audio, i)
+
+                        # Append the output audio file to the list for text file creation
+                        output_files.append("reverb_" + str(audio))
 
             elif augment_data_selected_option_index == 3:
                 output_files = []
