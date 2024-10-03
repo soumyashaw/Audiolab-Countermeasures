cc8e43c5b447f0691bc86253fa221ffa8aed8674
Soumya Shaw <37841510+soumyashaw@users.noreply.github.com>
2024-10-03 16:54:20 +0200

    Update VideoConferenceEffects.py

--- src/package_name/VideoConferenceEffects.py
+++ src/package_name/VideoConferenceEffects.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 
-def add_video_conferencing_perterbations_effects():
+def add_video_conferencing_perterbation_effects():
     print("Adding Video Conferencing Perturbations Effects")
