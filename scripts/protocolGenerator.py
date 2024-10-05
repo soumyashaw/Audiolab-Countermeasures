b9bdb8f6a47361d0c038ad8b54e7d29c39cfa301
Soumya Shaw <37841510+soumyashaw@users.noreply.github.com>
2024-10-05 10:45:40 +0200

    Update protocolGenerator.py

--- scripts/protocolGenerator.py
+++ scripts/protocolGenerator.py
@@ -12,3 +12,17 @@ with open("protocol.txt", "a") as f:
 
     for audio in spoofAudio:
         f.write(f"LA_0000 {audio.split('.')[0]} - - spoof\n")
+
+
+base_dir="/hkfs/home/haicore/hgf_cispa/hgf_yie2732/ASVspoof2019/LA/ASVspoof2019_LA_train/LibriSpeech/train-clean-100"
+
+# Set the destination folder where all audio files will be moved
+destination_folder="/hkfs/home/haicore/hgf_cispa/hgf_yie2732/ASVspoof2019/LA/ASVspoof2019_LA_train/LibriSpeech/train-clean-100/all"
+
+# Create the destination folder if it doesn't exist
+mkdir -p "$destination_folder"
+
+# Find and move all .flac and .wav files to the destination folder
+find "$base_dir" -type f \( -name "*.flac" -o -name "*.wav" \) -exec mv {} "$destination_folder" \;
+
+echo "All audio files have been moved to $destination_folder"
